e065de1ed9280dff47b6e0f91b5eaf5ab5748036
lelixone <smoke.torez@gmail.com>
2016-03-04 14:11:54 +0200

    sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,23 @@
 class UsersController < ApplicationController
-#  def index
-#  end
   def new
     @user = User.new
   end
 
+  def show
+    @user = User.all    
+  end
+
   def create
-    @user = User.new(params[:users])
+    @user = User.new(user_params)
     if @user.save
-        redirect_to root_url, notice: "Thank you for signing up"
+        redirect_to @user, notice: "Thank you for signing up"
     else
         render "new"
     end
+  end
+private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password,
+                                 :password_digest)  
   end
 end
